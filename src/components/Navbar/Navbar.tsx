a4dda8db0d3b9ee6e37159cbcee0e47034a5d762
barak <barak.create@gmail.com>
2024-02-23 11:07:46 +0200

    NavBar: IUpdate nav routing functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client"
-import {useRouter} from 'next/navigation'
-import {useState} from "react";
+import {usePathname, useRouter} from 'next/navigation'
 
 export interface NavItem {
   title: string,
@@ -12,20 +11,19 @@ interface Props {
 }
 
 export default function Navbar({navItem}: Props) {
+  const initialRoute = '/'
   const router = useRouter()
-
-  const [activeIndex, setActiveIndex] = useState(0);
+  const pathname = usePathname()
 
   return (
     <div className="inline-flex rounded-lg border border-gray-100 bg-gray-100 p-1">
       {
         navItem?.map((item, index) => {
-            const activeStyle = activeIndex === index ? 'bg-white text-blue-500 shadow-sm focus:relative' : ''
+            const activeStyle = pathname === item.route ? 'bg-white text-blue-500 shadow-sm focus:relative' : ''
             return <button
               key={item.title}
               className={`inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-gray-700 focus:relative ${activeStyle}`}
               onClick={() => {
-                setActiveIndex(index);
                 router.push(item.route)
               }}
             >
