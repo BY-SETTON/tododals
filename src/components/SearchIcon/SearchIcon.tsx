b2b3264eaac592b8a713cb2b50b11bc36b82912d
barak <barak.create@gmail.com>
2024-02-23 11:30:00 +0200

    TaskNoteColleciton: Sort by size, and style

--- src/components/SearchIcon/SearchIcon.tsx
+++ src/components/SearchIcon/SearchIcon.tsx
@@ -2,6 +2,5 @@ import * as Icons from "react-feather";
 
 export default function SearchIcon() {
   let icons = Object.keys(Icons);
-  console.log(icons);
   return (icons.map((icon) => (<div key={icon}>{icon}</div>)))
 }
