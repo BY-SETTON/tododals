2788d7573c02887d433f697b7adfd5478d6427f7
barak <barak.create@gmail.com>
2024-03-08 00:36:49 +0200

    login signup prevention

--- src/app/(auth)/signupLogin/LoginForm.tsx
+++ src/app/(auth)/signupLogin/LoginForm.tsx
@@ -3,12 +3,10 @@ import {Lock, User} from "react-feather";
 import {useFormState} from "react-dom";
 import {ResponseInterface} from "@/app/(authenticated)/new/(interfaces)/interface";
 import {ResponseTypes} from "@/app/(authenticated)/new/(enums)/(enums)";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import Card from "@/components/Card/Card";
-import Button from "@/components/Button/Button";
 import {login} from "@/app/(auth)/signupLogin/actions";
-import PopUp from "@/components/PopUp/PopUp";
 
 const initialState: ResponseInterface = {
   message: '',
@@ -16,6 +14,7 @@ const initialState: ResponseInterface = {
 }
 export default function LoginForm() {
   const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
   const [state, formAction] = useFormState(login, initialState);
 
   useEffect(() => {
@@ -27,10 +26,16 @@ export default function LoginForm() {
       default:
         break;
     }
+    setLoading(false)
+
   }, [state, router])
 
   return <Card className={"bg-neutral-100"}>
-    <form action={formAction} className="space-y-6">
+    <form action={formAction}
+          onSubmit={() => {
+            setLoading(true)
+          }}
+          className="space-y-6">
       <div className="flex flex-row justify-center items-center">
         <label htmlFor="login-username" className="sr-only">Username</label>
         <div className={" rounded-full mr-4 bg-neutral-500"}>
@@ -45,7 +50,7 @@ export default function LoginForm() {
         />
       </div>
       <div className="flex flex-row justify-center items-center">
-        <label htmlFor="password" className="sr-only">Password</label>
+        <label htmlFor="login-password" className="sr-only">Password</label>
         <div className={" rounded-full mr-4 bg-neutral-500"}>
           <Lock className="w-6 h-6 m-2 text-neutral-50"/>
         </div>
@@ -60,8 +65,9 @@ export default function LoginForm() {
       </div>
       <p>-{state.message}-</p>
       <div className="flex flex-row justify-center">
-        <button type="submit"
-                className={"bg-neutral-500 min-w-36 text-neutral-50 hover:bg-neutral-400 bg-primary-500-50  rounded-full p-3 transition-colors duration-300 ease-in-out"}>Login
+        <button type={loading ? "button" : "submit"}
+                className={"bg-neutral-500 min-w-36 text-neutral-50 hover:bg-neutral-400 bg-primary-500-50  rounded-full p-3 transition-colors duration-300 ease-in-out"}>
+          {loading ? 'Logging in' : 'Login'}
         </button>
       </div>
     </form>
--- src/app/(auth)/signupLogin/Signupform.tsx
+++ src/app/(auth)/signupLogin/Signupform.tsx
@@ -3,7 +3,7 @@ import {Lock, User} from "react-feather";
 import {useFormState} from "react-dom";
 import {ResponseInterface} from "@/app/(authenticated)/new/(interfaces)/interface";
 import {ResponseTypes} from "@/app/(authenticated)/new/(enums)/(enums)";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import Card from "@/components/Card/Card";
 import {signup} from "@/app/(auth)/signupLogin/actions";
@@ -14,6 +14,7 @@ const initialState: ResponseInterface = {
 }
 export default function SignupForm() {
   const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
   const [state, formAction] = useFormState(signup, initialState);
 
   useEffect(() => {
@@ -25,11 +26,13 @@ export default function SignupForm() {
       default:
         break;
     }
+    setLoading(false)
   }, [state, router])
 
   return <Card className={"bg-primary-500"}>
-    <form action={formAction}
-          className="space-y-6">
+    <form action={formAction} onSubmit={() => {
+      setLoading(true)
+    }} className="space-y-6">
       <div className="flex flex-row justify-center items-center">
         <label htmlFor="signup-username" className="sr-only">Username</label>
         <div className={" rounded-full mr-4 bg-primary-100"}>
@@ -58,9 +61,11 @@ export default function SignupForm() {
       </div>
       <p>-{state.message}-</p>
       <div className="flex flex-row justify-center">
-        <button type="submit"
-                className={"bg-primary-500-50  rounded-full p-3 transition-colors duration-300 ease-in-out bg-primary-100 min-w-36 text-primary-700 hover:bg-primary-50"}>Signup
+        <button type={loading ? "button" : "submit"}
+                className={"bg-primary-500-50  rounded-full p-3 transition-colors duration-300 ease-in-out bg-primary-100 min-w-36 text-primary-700 hover:bg-primary-50"}>
+          {loading ? 'Submitted' : 'Signup'}
         </button>
+
       </div>
     </form>
   </Card>;
